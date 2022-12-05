2b32cad6890d6ad8d233392a0893ae4826abb11e
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-12-05 23:06:56 +0530

    Get data from API

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
+
+function App() {
+  const APP_ID = "e50441f9";
+  const APP_KEY = "8f47262eef87ff79f6e77d1e14f06599";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="btn-serach" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
