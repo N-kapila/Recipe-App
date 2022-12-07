b2356c1452b1bd65ecc2f66aedd5f4a06de13bbf
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-12-08 00:05:59 +0530

    implemented search bar

--- src/App.js
+++ src/App.js
@@ -7,35 +7,55 @@ function App() {
   const APP_KEY = "8f47262eef87ff79f6e77d1e14f06599";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    //console.log(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
   };
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
         <button className="btn-serach" type="submit">
           Search
         </button>
       </form>
-      {recipes.map((recipe) => (
-        <Recipe
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import "./Recipe.css";
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} alt="" />
-    </div>
+    <Card style={{ width: "18rem" }} className="menu">
+      <Card.Title>{title}</Card.Title>
+      <br></br>
+      <Card.Img variant="top" src={image} className="image" />
+      <Card.Body>
+        <Card.Text>
+          <ol>
+            Ingredients:
+            {ingredients.map((ingredients) => (
+              <li>{ingredients.text}</li>
+            ))}
+          </ol>
+          <p>Calories:{calories}</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 export default Recipe;
